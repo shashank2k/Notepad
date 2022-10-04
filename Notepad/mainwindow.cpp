872498b8dcbb1a549badf40cdb45cabc5f37000e
shashank2k <75822772+shashank2k@users.noreply.github.com>
2022-10-04 11:27:27 +0530

    UI changes without button funtion

---
+++ Notepad/mainwindow.cpp
@@ -0,0 +1,70 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+
+}
+
